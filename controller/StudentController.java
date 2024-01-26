92daad8a40fc302ab700671bbba67361e1f347fc
NarayanSalitra06 <131778020+NarayanSalitra06@users.noreply.github.com>
2024-01-26 17:27:10 +0530

    Update StudentController.java

--- controller/StudentController.java
+++ controller/StudentController.java
@@ -3,17 +3,14 @@ package com.example.curdopratiom.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.example.curdopratiom.model.Student;
 import com.example.curdopratiom.service.StudentService;
 
 @RestController
